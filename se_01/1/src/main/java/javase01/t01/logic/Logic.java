62be4ee0598c8e77c28b4cfdbfe00acac8f594f3
Drazzilblol <paskalj10@gmail.com>
2015-10-04 10:59:34 +0300

    first

---
+++ se_01/1/src/main/java/javase01/t01/logic/Logic.java
@@ -0,0 +1,11 @@
+package javase01.t01.logic;
+
+/**
+ * Created by Drazz on 04.10.2015.
+ */
+public class Logic {
+  public String Method()
+  {
+    return "I am an string in logic";
+  }
+}
---
+++ se_01/1/src/main/java/javase01/t01/main/Main.java
@@ -0,0 +1,14 @@
+package javase01.t01.main;
+import javase01.t01.logic.Logic;
+
+/**
+ * Created by Drazz on 04.10.2015.
+ */
+public class Main {
+  public static void main(String[] args)
+  {
+    Logic logic = new Logic();
+    System.out.println(logic.Method());
+
+  }
+}
