e4bf9670bb020558b56e2ee703944b1805efa4a0
Drazzilblol <paskalj10@gmail.com>
2015-10-04 11:42:30 +0300

    second 2

--- se_01/2/src/main/java/javase01/t02/Main.java
+++ se_01/2/src/main/java/javase01/t02/Main.java
@@ -9,16 +9,20 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 50) * 2;
         }
+
+        System.out.println("original array");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.print('\n');
 
+        System.out.println("for");
         for (int i = 1; i < array.length; i += 2) {
             System.out.print(array[i] + array[i - 1] + " ");
         }
         System.out.print('\n');
 
+        System.out.println("while");
         int j = 1;
         while (j < array.length) {
             System.out.print(array[j] + array[j - 1] + " ");
@@ -26,6 +30,7 @@ public class Main {
         }
         System.out.print('\n');
 
+        System.out.println("do .... while");
         int k = 1;
         do {
             System.out.print(array[k] + array[k - 1] + " ");
