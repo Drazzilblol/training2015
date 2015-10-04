870b7154b89d439b661680ddcc63405e96cea0af
Drazzilblol <paskalj10@gmail.com>
2015-10-04 11:33:52 +0300

    first cc

--- se_01/1/src/main/java/javase01/t01/logic/Logic.java
+++ se_01/1/src/main/java/javase01/t01/logic/Logic.java
@@ -4,8 +4,7 @@ package javase01.t01.logic;
  * Created by Drazz on 04.10.2015.
  */
 public class Logic {
-  public String Method()
-  {
-    return "I am an string in logic";
-  }
+    public String Method() {
+        return "I am an string in logic";
+    }
 }
