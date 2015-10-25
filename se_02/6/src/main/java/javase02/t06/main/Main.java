e3f56a5825b31f20c78b2d12b34b8e3ceb7aaa1a
Drazzilblol <paskalj10@gmail.com>
2015-10-25 19:24:28 +0200

    inner clases

---
+++ se_02/6/src/main/java/javase02/t06/main/Main.java
@@ -0,0 +1,15 @@
+package javase02.t06.main;
+
+import javase02.t06.submarine.Submarine;
+
+/**
+ * Created by Drazz on 20.10.2015.
+ */
+public class Main {
+    public static void main(String ...args) {
+
+        Submarine submarine = new Submarine();
+       submarine.accelerate();
+
+    }
+}
