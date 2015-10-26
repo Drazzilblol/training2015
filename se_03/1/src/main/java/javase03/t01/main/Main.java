44d6b9c58200a302f43bd7c9220fd3d0ade44ceb
Drazzilblol <paskalj10@gmail.com>
2015-10-26 21:37:18 +0200

    loger v2

--- se_03/1/src/main/java/javase03/t01/main/Main.java
+++ se_03/1/src/main/java/javase03/t01/main/Main.java
@@ -10,21 +10,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String... args) {
         CrazyLogger cl = new CrazyLogger();
-        Scanner in = new Scanner(System.in);
-        while (in.hasNextLine()) {
-            String str = in.nextLine();
-            if (str.equals("")) break;
-            if (str.contains("FINDTEXT")) {
-                cl.findLogsByText(str.substring(str.indexOf("FINDTEXT") + 9));
-            } else if (str.contains("FINDTIME")) {
-                cl.findLogsByTime(str.substring(str.indexOf("FINDTIME") + 9));
-            } else if (str.contains("FIND")) {
-                cl.printFindedLogs(str.substring(str.indexOf("FIND") + 5));
-            } else if (str.contains("PRINT")) {
-                cl.printLog();
-            } else {
-                cl.log(str);
-            }
-        }
+        cl.run();
     }
 }
--- se_03/1/src/main/java/javase03/to1/logger/CrazyLogger.java
+++ se_03/1/src/main/java/javase03/to1/logger/CrazyLogger.java
@@ -4,56 +4,63 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Drazz on 20.10.2015.
  */
 public class CrazyLogger {
-    private List<StringBuilder> sbList = new ArrayList<>();
+    //  private List<StringBuilder> sbList = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
+
+    public void run(){
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            String str = in.nextLine();
+            if (str.equals("")) break;
+            if (str.contains("FINDTEXT")) {
+                findLogsByText(str.substring(str.indexOf("FINDTEXT") + 9));
+            } else if (str.contains("FINDTIME")) {
+                findLogsByTime(str.substring(str.indexOf("FINDTIME") + 9));
+            } else if (str.contains("FIND")) {
+                printFindedLogs(str.substring(str.indexOf("FIND") + 5));
+            } else if (str.contains("PRINT")) {
+                printLog();
+            } else {
+                log(str);
+            }
+        }
+    }
 
     public void log(String str) {
-        StringBuilder sb = new StringBuilder();
         String date = new SimpleDateFormat("dd-MM-YYYY : hh-mm-ss").format(new Date());
-        sb.append(date + " - " + str + '\n');
-        sbList.add(sb);
+        sb.append(date + " - " + str + ";\n");
     }
 
     public void printLog() {
-        for (StringBuilder sb : sbList) {
-            System.out.print(sb.toString());
-        }
-    }
-
-    public List<String> searchLogs(String str) {
-        List<String> stringList = new ArrayList<>();
-        for (StringBuilder sb : sbList) {
-            if (sb.indexOf(str) != -1) {
-                stringList.add(sb.toString());
-            }
-        }
-        return stringList;
+        System.out.println(sb.toString());
     }
 
     public void printFindedLogs(String str) {
-        for (StringBuilder sb : sbList) {
-            if (sb.indexOf(str) != -1) {
-                System.out.print(sb.toString());
+        for (String s : sb.toString().split("\n")) {
+            if (s.indexOf(str) != -1) {
+                System.out.println(s);
             }
         }
     }
 
     public void findLogsByText(String text) {
-        for (StringBuilder sb : sbList) {
-            if (sb.indexOf(text, 24) != -1) {
-                System.out.print(sb.toString());
+        for (String s : sb.toString().split("\n")) {
+            if (s.indexOf(text, 24) != -1) {
+                System.out.println(s);
             }
         }
     }
 
     public void findLogsByTime(String time) {
-        for (StringBuilder sb : sbList) {
-            if (sb.substring(0, 22).indexOf(time) != -1) {
-                System.out.print(sb.toString());
+        for (String s : sb.toString().split("\n")) {
+            if (s.substring(0, 22).indexOf(time) != -1) {
+                System.out.println(s);
             }
         }
     }
