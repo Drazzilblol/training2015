33766128e1d37407800cbc9f28195a66dcefd387
Drazzilblol <paskalj10@gmail.com>
2015-11-02 11:14:19 +0200

    b arr

--- se_04/1/src/main/java/javase04/t01/main/Main.java
+++ se_04/1/src/main/java/javase04/t01/main/Main.java
@@ -1,13 +1,13 @@
 package javase04.t01.main;
 
-import javase04.t01.reader.Reader;
+import javase04.t01.reader.ByteReader;
 
 /**
  * Created by Drazz on 02.11.2015.
  */
 public class Main {
     public static void main(String... args) {
-        new Reader().readFile();
+        new ByteReader().readFile();
 
     }
 }
