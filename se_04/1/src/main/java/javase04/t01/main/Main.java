dd7b52a2447c018272ac92247c8350ffb5a66241
Drazzilblol <paskalj10@gmail.com>
2015-11-02 10:56:45 +0200

    byte streams

---
+++ se_04/1/src/main/java/javase04/t01/main/Main.java
@@ -0,0 +1,13 @@
+package javase04.t01.main;
+
+import javase04.t01.reader.Reader;
+
+/**
+ * Created by Drazz on 02.11.2015.
+ */
+public class Main {
+    public static void main(String... args) {
+        new Reader().readFile();
+
+    }
+}
